e32957d619c0e2dc596f085e3f6b2a3c761ba3e1
Deng <1592310935@qq.com>
2020-03-26 16:26:12 +0800

    测试,提交TestGit

---
+++ day44__Demo01_git/src/main/java/com/my/TestGit.java
@@ -0,0 +1,12 @@
+package com.my;
+
+/**
+ * @Author: Deng
+ * @date: 2020-03-26 16:23
+ * @description:
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("测试Git");
+    }
+}
